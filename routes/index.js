0871d417dfafd203400350230eb526ff42a1dd07
Reejh Ghosh <Reejh Ghosh>
2017-03-28 23:13:56 +0530

    updates

--- routes/index.js
+++ routes/index.js
@@ -2,31 +2,29 @@ var express = require('express');
 var router = express.Router();
 
 var latest =new Object;
+for(var i=0;i<4;i++){
+	latest[i]={};
+	latest[i]['col']='R';
+	latest[i]['lno']=i.toString();
+	latest[i]['noc']=0;
+}
 
 router.post('/post',function(req,res){
-	//console.log(req.body);
-	latest[req.body.lno]={};
-    latest[req.body.lno]['col'] =  req.body.col;
-    latest[req.body.lno]['noc'] = req.body.noc;
+    latest[req.body.lno]['col'] = req.body.col;
     latest[req.body.lno]['lno']= req.body.lno;
-    latest[req.body.lno]['status'] = 'change';
     res.send({'message':'success'});
 });
 
+router.post('/post_noc',function(req,res){
+	for(var i=0;i<4;i++){
+		latest[i]['noc']=req.body[i];
+	}
+	res.send({'message':'success'})
+});
+
 router.get('/get',function(req,res){
-    var send={};
-    //res.send(Object.keys(latest).length.toString());
-    var c=0;
-    for(var i in latest){
-		console.log(latest[i]);
-        if(latest[i]['status']=="change"){
-			send[latest[i]['lno']]=latest[i];
-			latest[i].status="sent";
-		}
-        if(c==parseInt(Object.keys(latest).length.toString())-1)
-        res.send(send);
-        c++;
-    }
+    var send=latest;
+    res.send(send);
 });
 
 router.get('/',function(req,res){
